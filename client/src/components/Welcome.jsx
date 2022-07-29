d5cd810e4d32641f1973fead9f094633894f1d1a
Om Bhatnagar <omishb2001@gmail.com>
2022-07-29 21:27:57 +0530

    Added ethereum transfer functionality.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,9 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
 import { Loader } from "./";
+import { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
 
 const commonStyles =
 	"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -19,8 +20,21 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-	const connectWallet = () => {};
-	const handleSubmit = () => {};
+	const {
+		connectWallet,
+		currentAccount,
+		formData,
+		handleChange,
+		sendTransaction,
+	} = useContext(TransactionContext);
+
+	const handleSubmit = (e) => {
+		const { addressTo, amount, keyword, message } = formData;
+		e.preventDefault();
+
+		if (!addressTo || !amount || !keyword || !message) return;
+		sendTransaction();
+	};
 
 	return (
 		<div className='flex w-full justify-center items-center'>
@@ -33,13 +47,17 @@ const Welcome = () => {
 						Explore the crypto world. Buy and sell crypto-currencies easily on
 						Krypto
 					</p>
-					<button
-						type='button'
-						onClick={connectWallet}
-						className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
-					>
-						<p className='text-white text-base font-semibold'>Connect Wallet</p>
-					</button>
+					{!currentAccount && (
+						<button
+							type='button'
+							onClick={connectWallet}
+							className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+						>
+							<p className='text-white text-base font-semibold'>
+								Connect Wallet
+							</p>
+						</button>
+					)}
 					<div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
 						<div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
 						<div className={commonStyles}>Security</div>
@@ -69,27 +87,27 @@ const Welcome = () => {
 					<div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
 						<Input
 							placeholder='Address To'
-							name='addessTo'
+							name='addressTo'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Amount (ETH)'
 							name='amount'
 							type='number'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Keyword (GIF)'
 							name='keyword'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder='Enter message'
 							name='message'
 							type='text'
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<div className='h-[1px] w-full bg-gray-400 my-2' />
 						{false ? (
---
+++ client/src/utils/constans.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x2b2bBD34703dc576fa4e5Dfb48F8A8d504C45298";
