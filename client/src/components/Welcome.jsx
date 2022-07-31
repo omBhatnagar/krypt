dd92971816e8e71bd8d9a7901153d66a37665b8f
Om Bhatnagar <omishb2001@gmail.com>
2022-07-31 18:08:22 +0530

    Front end finished.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,7 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 import { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 
 const commonStyles =
 	"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -68,7 +69,7 @@ const Welcome = () => {
 					</div>
 				</div>
 				<div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-					<div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+					<div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card'>
 						<div className='flex justify-between flex-col w-full h-full'>
 							<div className='flex justify-between items-start'>
 								<div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
@@ -77,7 +78,9 @@ const Welcome = () => {
 								<BsInfoCircle fontSize={17} color='#fff' />
 							</div>
 							<div>
-								<p className='text-white font-light text-sm'>Address</p>
+								<p className='text-white font-light text-sm'>
+									{shortenAddress(currentAccount)}
+								</p>
 								<p className='text-white font-semibold text-lg mt-1'>
 									Ethereum
 								</p>
